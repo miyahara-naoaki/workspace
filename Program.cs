d45a4d675694bcdd9aaf4f41e1e9736291cba81c
miyahara-naoaki <galaxy7rush@gmail.com>
2024-12-26 12:51:01 +0900

    初期プロジェクトファイルと構成を追加

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// namespace workspace;
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.WriteLine("HelloHello, World!");
+//     }
+// }
---
+++ Test.cs
@@ -0,0 +1,19 @@
+namespace workspace;
+class Test
+{
+    static void Main(string[] args)
+    {
+        string flg = "No";
+        string input1 = Console.ReadLine();
+        string input2 = Console.ReadLine();
+        string[] stringArray1 = input1.Split(' ');
+        string[] stringArray2 = input2.Split(' ');
+        for (int i = 0 ;i < int.Parse(stringArray1[0]); i++){
+            if (stringArray1[1].Equals(stringArray2[i])) {
+                flg = "Yes";
+                break;
+            }
+        }
+        Console.WriteLine(flg);
+    }
+}
